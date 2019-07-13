544030b8efb03e297605974c20011fa5db2cb859
pabsusdev7 <pabsusdrumer@gmail.com>
2019-07-13 13:26:29 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Pablo Albuja on 7/13/19.
+//  Copyright © 2019 Ingenuity Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
